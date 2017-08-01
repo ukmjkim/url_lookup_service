c65a8ce2f5d854296d3e11db1223d2c371040f84
EC2 Default User <ec2-user@ip-172-31-42-226.us-east-2.compute.internal>
2017-08-01 17:26:25 +0000

    Modified routes for non-resourceful routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :urlinfos do
   end
+  get 'urlinfo/1' => 'urlinfos#index'
+  get 'urlinfo/1/:domain_name/:query_string' => 'urlinfos#show'
+  post 'urlinfo/1/:domain_name/:query_string' => 'urlinfos#post'
+  delete 'urlinfo/1/:domain_name/:query_string' => 'urlinfos#delete'
 end
