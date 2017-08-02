51782e985035539222d649335811206c72c6913d
EC2 Default User <ec2-user@ip-172-31-42-226.us-east-2.compute.internal>
2017-08-02 21:51:11 +0000

    Installed Rubocop and SimpleCov for code coverage and style check

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,10 @@
 module UrlLookupCache
   class << self
     def redis
-      REDIS_INSTANCE_LIST.each do |key, redis_instance|
-        redis_instance[:object] ||= Redis.new(:url => (redis_instance[:url] || 'redis://127.0.0.1:6379'))
+      REDIS_INSTANCE_LIST.each do |_key, redis_instance|
+        redis_instance[:object] ||= Redis.new(
+          :url => (redis_instance[:url] || 'redis://127.0.0.1:6379')
+        )
       end
       REDIS_INSTANCE_LIST
     end
